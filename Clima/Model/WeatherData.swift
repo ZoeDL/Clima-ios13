f5dc61075fe81cb4042ac9000c5a87f3ec9b9e0d
zexi yu <eezxyu@gmail.com>
2020-03-22 12:47:52 +0800

    Create a decodable struct according to the JSON structure. Use Decoder to decode the Data and extract certain fields.

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by 余泽熙 on 3/22/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+}
